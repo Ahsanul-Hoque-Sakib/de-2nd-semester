a83724728d70f5cab80ebaec07be8a6451a9266c
Ahsanul-Hoque-Sakib <30299266+Ahsanul-Hoque-Sakib@users.noreply.github.com>
2017-10-11 23:51:53 +0600

    Add files via upload

---
+++ sort in ascending order.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+main(){
+	
+	int a[15];
+	int i,j,n,swap=0;
+	
+	printf("How many elements:\n");
+	scanf("%d",&n);
+	
+	printf("Enter the elements:\n");
+	for(i=1;i<=n;i++){
+		scanf("%d",&a[i]);
+	}
+	
+		printf("The array is:\n");
+	for(i=1;i<=n;i++){
+		printf("%d",a[i]);
+	}
+	
+	for(i=1;i<=n;i++){
+		for(j=i+1;j<=n;j++){
+			if(a[i]>a[j]){
+				swap=a[i];
+				a[i]=a[j];
+				a[j]=swap;
+			}
+			
+		}
+	}
+	
+	printf("The sorted array in Ascending:\n");
+	for(i=1;i<=n;i++){
+		printf("%d\t",a[i]);
+	
+	}
+}
